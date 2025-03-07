402cd1329d6988dff2e76e9e3456c5f9c4c6c334
Anna Chulaki <anna.chulaki@gmail.com>
2025-03-07 14:21:38 -0500

    Added new code segment

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
+print("Hello Earth")
+print()
 print("This is my first Git repository on GitHub.")
 def print_message (name='GitHub'):
      print("This is my first Git repository on GitHub.")
 print_message()
+
